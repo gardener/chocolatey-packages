6b64afea98122e171880d4696172653128ed11af
Lukas Gross <lukas.gross@sap.com>
2021-08-06 16:46:57 +0200

    Fixed LICENSE reuse

--- gardenlogin/tools/chocolateyinstall.ps1
+++ gardenlogin/tools/chocolateyinstall.ps1
@@ -1,4 +1,8 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+$ErrorActionPreference = 'Stop'
 $url64      = "https://github.com/gardener/gardenlogin/releases/download/$env:chocolateyPackageVersion/gardenlogin_windows_amd64.exe"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checksum64 = Get-Content "$toolsDir\checksum64.txt" -First 1
