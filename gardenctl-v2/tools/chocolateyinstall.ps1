ee240a23c3cb205115af4cb1a5679c67fee6bd58
gardener-robot-ci-1 <gardener.ci.user@gmail.com>
2022-02-24 13:40:57 +0000

    Update gardenctl-v2

--- gardenctl-v2/tools/chocolateyinstall.ps1
+++ gardenctl-v2/tools/chocolateyinstall.ps1
@@ -1,17 +1,12 @@
-﻿# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
 $ErrorActionPreference = 'Stop'
-$url64      = "https://github.com/gardener/gardenctl-v2/releases/download/0.0.0/gardenctl_v2_windows_amd64.exe"
+$url64      = "https://github.com/gardener/gardenctl-v2/releases/download/v2.0.0/gardenctl_v2_windows_amd64.exe"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
   Url64bit        = $url64
   ChecksumType64  = 'sha256'
-  Checksum64      = ""
+  Checksum64      = "e94639f849b4e4678b80af0b8b86fa208f20dd0556302eae9e9cc704ba5884c2"
   FileFullPath    = "$toolsDir\gardenctl-v2.exe"
 }
-
 Get-ChocolateyWebFile @packageArgs
