d7c178c262685f3c4c58a3992d34c48e530334b6
gardener-robot-ci-1 <gardener.ci.user@gmail.com>
2022-07-12 18:32:17 +0000

    Update gardenlogin

--- gardenlogin/tools/chocolateyinstall.ps1
+++ gardenlogin/tools/chocolateyinstall.ps1
@@ -1,19 +1,14 @@
-﻿# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
 $ErrorActionPreference = 'Stop'
-$url64      = "https://github.com/gardener/gardenlogin/releases/download/0.0.0/gardenlogin_windows_amd64.exe"
+$url64      = "https://github.com/gardener/gardenlogin/releases/download/v0.3.0/gardenlogin_windows_amd64.exe"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
   Url64bit        = $url64
   ChecksumType64  = 'sha256'
-  Checksum64      = ""
+  Checksum64      = "caff78d41f64d7a3383c0538612d2bd616e679bb37a8691bc55138158511f251"
   FileFullPath    = "$toolsDir\gardenlogin.exe"
 }
-
 Get-ChocolateyWebFile @packageArgs
-
 Install-BinFile -Name "kubectl-gardenlogin" -Path "$toolsDir\gardenlogin.exe"
+
--- gardenlogin/tools/chocolateyuninstall.ps1
+++ gardenlogin/tools/chocolateyuninstall.ps1
@@ -1,5 +1,1 @@
-﻿# SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
 Uninstall-BinFile -Name "kubectl-gardenlogin"
