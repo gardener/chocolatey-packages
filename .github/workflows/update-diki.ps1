6518da6238f009e34716a7e6ff98a11d8fe87e29
Aleksandar Savchev <aleksandar.savchev@sap.com>
2025-03-11 15:14:10 +0200

    Push diki package

---
+++ .github/workflows/update-diki.ps1
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+Param(
+  [Parameter(Mandatory=$true)]
+  [string]$version,
+  [Parameter(Mandatory=$true)]
+  [string]$checksum64
+)
+
+@"	
+`$ErrorActionPreference = 'Stop'
+`$url64      = "https://github.com/gardener/diki/releases/download/$version/diki-windows-amd64"
+`$toolsDir   = "`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)"
+
+`$packageArgs = @{
+  PackageName     = `$env:ChocolateyPackageName
+  Url64bit        = `$url64
+  ChecksumType64  = 'sha256'
+  Checksum64      = "$checksum64"
+  FileFullPath    = "`$toolsDir\diki.exe"
+}
+Get-ChocolateyWebFile @packageArgs
+"@ > diki\tools\chocolateyinstall.ps1
